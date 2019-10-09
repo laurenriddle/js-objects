278a8c64583837f6380970b0a89ce98fa548afe7
Lauren Riddle <lari7132@gmail.com>
2019-10-09 15:05:49 -0500

    added js code for lightning exercises round 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,66 @@
-console.log("We're learning about objects!")
+// console.log("We're learning about objects!")
 
 
-// Lightning Exercise 1: Given two options of data structures - array or object - which should you use? You need to represent a red 2015 Ford Mustang in your code.
+// // Lightning Exercise 1: Given two options of data structures - array or object - which should you use? You need to represent a red 2015 Ford Mustang in your code.
 
-const vehicle = {
-    color: "Red",
-    year: 2015,
-    make: "Ford",
-    model: "Mustang"
+// const vehicle = {
+//     color: "Red",
+//     year: 2015,
+//     make: "Ford",
+//     model: "Mustang"
+// }
+
+
+
+// // Lightning Exercise 2: Given two options of data structures - array or object - which should you use? You need to store list of animal names in a shelter. The names are "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", and "Sharky".
+
+// const animalNames = [ "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky" ]
+
+
+
+// // Lightning Exercise 3: Define an object for four family members, and put each object in an array named familyMembers. Each family member object that you create should have the same keys on them, but the values will be different.
+
+// const father = {
+//     name: "Mike",
+//     age: 48
+// }
+
+// const brother = {
+//     name: "Caleb",
+//     age: 14
+// }
+
+// const sister = {
+//     name: "Gabi",
+//     age: 10
+// }
+
+// const mother = {
+//     name: "Virginia",
+//     age: 42
+// }
+
+// const familyMembers = [father, brother, sister, mother]
+
+// console.log("familyMembers", familyMembers)
+
+
+
+
+
+
+
+
+
+// Lightning Exercise: Given the object below, output each of the values to the console using dot notation.
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
 }
 
 
-
-// Lightning Exercise 2: Given two options of data structures - array or object - which should you use? You need to store list of animal names in a shelter. The names are "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", and "Sharky".
-
-const animalNames = [ "Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky" ]
-
-
-
-// Lightning Exercise 3: Define an object for four family members, and put each object in an array named familyMembers. Each family member object that you create should have the same keys on them, but the values will be different.
-
-const father = {
-    name: "Mike",
-    age: 48
-}
-
-const brother = {
-    name: "Caleb",
-    age: 14
-}
-
-const sister = {
-    name: "Gabi",
-    age: 10
-}
-
-const mother = {
-    name: "Virginia",
-    age: 42
-}
-
-const familyMembers = [father, brother, sister, mother]
-
-console.log("familyMembers", familyMembers)
+console.log("These are the values of wardrobe:", wardrobe.height, wardrobe.manufacturer, wardrobe.contents, wardrobe.depth, wardrobe.width)
